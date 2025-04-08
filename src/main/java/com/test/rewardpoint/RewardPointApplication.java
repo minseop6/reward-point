9569ac8f22d99ade49f01e8295a8dcde02dd711b
minseop6 <gim34@naver.com>
2025-04-08 21:00:12 +0900

    chore: initialize

---
+++ src/main/java/com/test/rewardpoint/RewardPointApplication.java
@@ -0,0 +1,13 @@
+package com.test.rewardpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardPointApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RewardPointApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/rewardpoint/RewardPointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.rewardpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardPointApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
